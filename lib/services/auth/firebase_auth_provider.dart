f3fc41825a9d92cd4d018fb6242852c65805fb49
eleuvete <villa@vecinapp.org>
2024-08-23 13:56:19 -0600

    auth service initialization

--- lib/services/auth/firebase_auth_provider.dart
+++ lib/services/auth/firebase_auth_provider.dart
@@ -2,10 +2,18 @@ import 'package:vecinapp/services/auth/auth_user.dart';
 import 'package:vecinapp/services/auth/auth_provider.dart';
 import 'package:vecinapp/services/auth/auth_exceptions.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:vecinapp/firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart'
     show FirebaseAuth, FirebaseAuthException;
 
 class FirebaseAuthProvider implements AuthProvider {
+  @override
+  Future<void> initialize() async {
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
+  }
+
   @override
   Future<AuthUser> createUser({
     required String email,
