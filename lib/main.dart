ee7aea31a7d2a860639bd0a3c067e808b0e0459e
eleuvete <villa@vecinapp.org>
2024-10-09 21:20:17 -0600

    cloud provider in app bloc 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:vecinapp/firebase_options.dart';
-import 'package:vecinapp/views/app.dart';
+import 'package:vecinapp/views/vecinapp.dart';
 import 'services/settings/settings_controller.dart';
 
 void main() async {
